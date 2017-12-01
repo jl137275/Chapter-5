05f4068051375c46e45de52ab636f90837a88afd
jl137275 <33095838+jl137275@users.noreply.github.com>
2017-12-01 10:59:54 -0600

    renamed chapter 5 to coin toss

---
+++ Coin toss/Chapter 5/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Dim headsTotal As Integer
+    Dim tailTotal As Integer
+    Private Sub btnEnterSales_Click(sender As Object, e As EventArgs) Handles btnToss.Click
+        Dim intSideup As Integer
+
+        Dim rand As New Random
+
+        intSideup = rand.Next(2)
+        If intSideup = 0 Then
+            picTails.Visible = True
+            picHeads.Visible = False
+            tailTotal = 1 + tailTotal
+
+        Else
+            picHeads.Visible = True
+            picTails.Visible = False
+            headsTotal = 1 + headsTotal
+        End If
+
+        lblHeadsTotal.Text = headsTotal.ToString
+        lblTailsTotal.Text = tailTotal.ToString
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
