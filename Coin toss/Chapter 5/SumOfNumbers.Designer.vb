db2d56132311f4affeb279be2442f4abbe5a89c6
jl137275 <33095838+jl137275@users.noreply.github.com>
2017-12-06 10:29:37 -0600

    started sum of numbers

---
+++ Coin toss/Chapter 5/SumOfNumbers.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SumOfNumbers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEnterNumbers = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnEnterNumbers
+        '
+        Me.btnEnterNumbers.Location = New System.Drawing.Point(29, 59)
+        Me.btnEnterNumbers.Name = "btnEnterNumbers"
+        Me.btnEnterNumbers.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnterNumbers.TabIndex = 0
+        Me.btnEnterNumbers.Text = "Enter Numbers"
+        Me.btnEnterNumbers.UseVisualStyleBackColor = True
+        '
+        'SumOfNumbers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.btnEnterNumbers)
+        Me.Name = "SumOfNumbers"
+        Me.Text = "SumOfNumbers"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnEnterNumbers As System.Windows.Forms.Button
+End Class
---
+++ Coin toss/Chapter 5/SumOfNumbers.vb
@@ -0,0 +1,3 @@
+﻿Public Class SumOfNumbers
+
+End Class
