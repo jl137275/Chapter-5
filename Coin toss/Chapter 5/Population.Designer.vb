687d536d7e6de4be2ae35ae0def0099fd80376e9
jl137275 <33095838+jl137275@users.noreply.github.com>
2017-12-11 11:35:38 -0600

    update population

--- Coin toss/Chapter 5/Population.Designer.vb
+++ Coin toss/Chapter 5/Population.Designer.vb
@@ -22,39 +22,96 @@ Partial Class Population
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.cboDays = New System.Windows.Forms.ComboBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.txtIncreasePct = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'ComboBox1
+        'cboDays
         '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(1, 21)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(113, 21)
-        Me.ComboBox1.TabIndex = 0
+        Me.cboDays.FormattingEnabled = True
+        Me.cboDays.Location = New System.Drawing.Point(12, 36)
+        Me.cboDays.Name = "cboDays"
+        Me.cboDays.Size = New System.Drawing.Size(113, 21)
+        Me.cboDays.TabIndex = 0
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 97)
+        Me.Button1.Location = New System.Drawing.Point(25, 250)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.Location = New System.Drawing.Point(12, 113)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(353, 95)
+        Me.lstOutput.TabIndex = 2
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(244, 36)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox2.TabIndex = 3
+        '
+        'txtIncreasePct
+        '
+        Me.txtIncreasePct.Location = New System.Drawing.Point(138, 37)
+        Me.txtIncreasePct.Name = "txtIncreasePct"
+        Me.txtIncreasePct.Size = New System.Drawing.Size(100, 20)
+        Me.txtIncreasePct.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(152, 250)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(273, 250)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
         'Population
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ClientSize = New System.Drawing.Size(377, 294)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.txtIncreasePct)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.lstOutput)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.cboDays)
         Me.Name = "Population"
         Me.Text = "Population"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents cboDays As System.Windows.Forms.ComboBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents lstOutput As System.Windows.Forms.ListBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents txtIncreasePct As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
 End Class
--- Coin toss/Chapter 5/Population.vb
+++ Coin toss/Chapter 5/Population.vb
@@ -1,10 +1,23 @@
 ﻿Public Class Population
 
-    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboDays.SelectedIndexChanged
 
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intcount As Integer
+        Dim dbltotal As Double
+        lstOutput.Items.Add("Day Approximate Population")
+        lstOutput.Items.Add("--------------------------")
+        dbltotal = 2
+        intcount = 1
+
+        lstOutput.Items.Add(intcount.ToString + " " + )
+
+        For intcount = intcount + 1 To cboDays.Text
+            dbltotal = (CDbl(txtIncreasePct.Text * dbltotal))
+            lstOutput.Items.Add(intcount.ToString + "")
+        Next
 
     End Sub
 End Class
